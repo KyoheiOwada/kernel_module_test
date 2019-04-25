3f8bfba5a99f380702c9f811285c232af0157c66
Kyohei Owada <44285607+KyoheiOwada@users.noreply.github.com>
2019-04-25 20:13:59 +0900

    Update and rename ledTest.c to lednipple.c

--- lednipple.c
+++ lednipple.c
@@ -1,5 +1,5 @@
 /*
- * ledTest.c
+ * lednipple.c
  * This is Device driver to handle LEDs for Raspberry Pi.
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
